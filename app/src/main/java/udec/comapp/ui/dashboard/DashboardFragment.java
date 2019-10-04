2146f321b143205b755d26401af0767c2903c8b3
Angelo <angelozapata@udec.cl>
2019-10-04 16:34:57 -0300

    Botones añadidos.

--- app/src/main/java/udec/comapp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/udec/comapp/ui/dashboard/DashboardFragment.java
@@ -1,9 +1,12 @@
 package udec.comapp.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,23 +16,25 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import udec.comapp.R;
+import udec.comapp.vendedor;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
+        final Button ven = root.findViewById(R.id.button);
+        ven.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), vendedor.class);
+                startActivity(i);
+
             }
         });
+
         return root;
     }
 }
