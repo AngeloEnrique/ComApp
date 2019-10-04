95a14fae7d2605b8a6ebb506bf9f8207c0e7c447
mmonjeudec <mmonje@udec.cl>
2019-10-03 21:20:59 -0300

    Arregle algunos detalles e hice un primer metodo para insertar productos a la base

--- app/src/main/java/udec/comapp/DBHelper.java
+++ app/src/main/java/udec/comapp/DBHelper.java
@@ -13,16 +13,30 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table vendedores(username text primary key, contraseña text, nombre text, operando boolean, localizacion text)");
+        db.execSQL("create table vendedores(username text primary key, contraseña text, nombre text, operando int, localizacion text)");
         db.execSQL("create table clientes(username text primary key, contraseña text)");
-        db.execSQL("create table productos(id int primary key, nombre text, precio real, disponible boolean, vendedor text, foreign key(vendedor) references vendedores(username))");
-        db.execSQL("create table ofertas(id int primary key, descuento real, duracion int, vendedor text, id_prod int, foreign key(vendedor) references vendedores(username), foreign key(id_prod) references productos(id))");
+        db.execSQL("create table productos(id integer primary key, nombre text, precio int, disponible int, vendedor text, foreign key(vendedor) references vendedores(username))");
+        db.execSQL("create table ofertas(id integer primary key, descuento int, duracion int, vendedor text, id_prod int, foreign key(vendedor) references vendedores(username), foreign key(id_prod) references productos(id))");
 
-        db.execSQL("insert into vendedores values('ccastro', '1234', 'Cristian Castro', True, 'Central')");
+        db.execSQL("insert into vendedores (username, contraseña, nombre, operando, localizacion) values('ccastro', '1234', 'Cristian Castro', 1, 'Central')");
+        db.execSQL("insert into vendedores (username, contraseña, nombre, operando, localizacion) values('vivimechada', '1234', 'Viviana', 0, 'Foro')");
+        db.execSQL("insert into clientes (username, contraseña)values('grojas', '1234')");
+        db.execSQL("insert into productos (nombre, precio, disponible, vendedor) values('Hamburguesa Simple', '1500', 1, 'ccastro')");
+        db.execSQL("insert into productos (nombre, precio, disponible, vendedor) values('Hamburguesa Soya', '1500', 1, 'ccastro')");
+        db.execSQL("insert into productos (nombre, precio, disponible, vendedor) values('Sandwich Mechada Palta', '1300', 0, 'vivimechada')");
+        db.execSQL("insert into productos (nombre, precio, disponible, vendedor) values('Sandwich Mechada Queso', '1300', 0, 'vivimechada')");
+        db.execSQL("insert into ofertas (descuento, duracion, vendedor, id_prod)values('500', '30', 'ccastro', 2)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void insertarProducto(SQLiteDatabase db, String nombre, int precio, int disponible, String vendedor){
+
+        db.execSQL("insert into productos (nombre, precio, disponible, vendedor) values("+nombre+", "+precio+", "+disponible+", "+vendedor+")");
 
     }
 }
