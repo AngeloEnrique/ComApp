d7d6ebcb26ffd2ed0b8c74d0064027ca4e5a9c35
jhgaldames <estefanoesgenial@gmail.com>
2019-10-04 16:06:08 -0300

    iconos agregados

--- app/src/main/java/udec/comapp/ui/ProfileFragment.java
+++ app/src/main/java/udec/comapp/ui/ProfileFragment.java
@@ -15,7 +15,11 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.ven_perfil, container, false);
+        final View view = inflater.inflate(R.layout.ven_perfil, container, false);
+
+
+
+        return view;
     }
 }
 
